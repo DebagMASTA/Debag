e803b204375cab24e9125c8acddc79c30df38e4e
Mitsuru Yambe <43664022+DebagMASTA@users.noreply.github.com>
2018-10-01 15:38:11 +0900

    Add files via upload

---
+++ debag/debag/debag.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"debag.h"
+//void debag()
+//{
+//	
+//		printf("x = -2 : ラベル = -1\nx = -0.51 : ラベル = -1\nx = -0.49 : ラベル = 1\nx = 1 : ラベル = 1\n");
+//		
+//		(void)getchar();
+//		(void)getchar();
+//		
+//	
+//}
+
+
+
+int main() {
+	debag();
+	return 0;
+}
---
+++ debag/debag/debag.h
@@ -0,0 +1,6 @@
+void debag()
+{
+	printf("x = -2 : ラベル = -1\nx = -0.51 : ラベル = -1\nx = -0.49 : ラベル = 1\nx = 1 : ラベル = 1\n");
+	(void)getchar();
+	(void)getchar();
+}
